95f8cd1e20f34abf7cc92120bd1d929764d108af
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-01-29 14:33:51 +0100

    Add files via upload

---
+++ Ejercicio03.Fechas.Programacion/Ejercicio03.Fechas.Programacion/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ejercicio03.Fechas.Programacion;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        DateTime fechaActual = DateTime.Now;
+        Console.WriteLine(fechaActual.ToString());
+
+        DateTime fechaModificada = DateTime.Now;
+
+
+        fechaModificada = fechaModificada.AddDays(1).AddMonths(-24) ;
+        Console.WriteLine(fechaModificada.ToString());
+
+
+        Random rnd = new Random();
+        int numeroAleatorio = rnd.Next(4);
+
+        fechaModificada = fechaModificada.AddYears(numeroAleatorio);
+        Console.WriteLine(fechaModificada.ToString());
+
+
+        //Comparativa de las fechas
+        if (fechaActual.CompareTo(fechaModificada) >0)
+        {
+
+            Console.WriteLine("El hoy es mas Grande: ");
+
+
+        }
+
+        else
+        {
+            Console.WriteLine("El hoy no es mas grande");
+        }
+
+
+
+
+    }
+
+
+}
